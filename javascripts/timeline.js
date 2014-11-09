2f57b702948642be742f30d4cf41228ca368486e
Daniel Tamai <daniel.tamai@gmail.com>
2014-11-09 15:03:19 -0200

    Enables unstacking the boxes
    When unstacked the boxes take as few lines as possible so that they do
    not overlap.

--- javascripts/timeline.js
+++ javascripts/timeline.js
@@ -43,34 +43,45 @@ load = function(source) {
       return;
     }
     x.domain([0, 30]);
+    rows.forEach(function(d) {
+      d.startX = x(dayOfYearFormat(d.dateStarted));
+      d.width = x(d.readingDuration);
+      d.endX = d.startX + d.width;
+    });
+    rows.sort(function(a, b) {
+      return a.dateStarted - b.dateStarted;
+    });
+    lanes = [];
+    laneFor = function(d) {
+      var lastLane = lanes.length - 1;
+      for(i = 0; i <= lastLane; i++) {
+        if (d.startX >= lanes[i]) {
+          return i;
+        }
+      }
+      return lastLane + 1;
+    };
+    rows.forEach(function(d, i, ary) {
+      lane = laneFor(d);
+      d.lane = lane;
+      lanes[lane] = d.endX;
+    });
 
     var svgRoot = d3.select(".boxes")
         .attr("width", width)
         .attr("height", 50 + height * rows.length);
 
-    var bar = svgRoot.selectAll("g.box")
+    var bar = svgRoot.selectAll(".box")
         .data(rows)
-      .enter().append("g")
-        .attr("transform", function(d, i) {
-          return "translate(" + (50 + x(dayOfYearFormat(d.dateStarted))) + ", " + (i * height + 40) + ")";
-        })
-
-    bar.classed("box", true);
-    bar.classed("in-progress", function(d) { return d.inProgress; });
-
-    bar.append("rect")
-        .attr("width", function(d) { return x(d.readingDuration); })
-        .attr("height", height - 2)
+      .enter().append("rect")
+        .attr("class", function(d) { return "box " + (d.inProgress ? "in-progress" : ""); })
+        .attr("width", function(d) { return d.width; })
+        .attr("height", height - 3)
+        .attr("x", function(d) { return 50 + d.startX; })
+        .attr("y", function(d) { return 40 + d.lane * height; })
         .on("mouseover", mouseover)
         .on("mousemove", mousemove)
         .on("mouseout", mouseout);
-
-    bar.append("text")
-        .attr("x", 5)
-        .attr("y", height/2)
-        .attr("dy", ".25em")
-        //.text(function(d) { return d.title; });
-        ;
 
     var tooltip = d3.select("body").append("div")
         .attr("class", "tooltip")
@@ -100,6 +111,28 @@ load = function(source) {
       tooltip.transition()
           .duration(500)
           .style("opacity", 1e-6);
+    }
+
+    d3.select("input").on("change", change);
+
+    var sortTimeout = setTimeout(function() {
+      d3.select("input").property("checked", true).each(change);
+    }, 2000);
+
+    function change() {
+      clearTimeout(sortTimeout);
+      var transition = svgRoot.transition().duration(750);
+
+      var doStack = this.checked;
+
+      transition.selectAll(".box")
+        .delay(function(d, i) { return i * 30; })
+        .attr("y", function(d, i) {
+          var posY = doStack
+            ? height * i
+            : height * d.lane;
+          return 40 + posY;
+        });
     }
 
   });
